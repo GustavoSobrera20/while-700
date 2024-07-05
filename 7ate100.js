8e3a59cead565b43e68bfbbb15ed6ed2fd804735
GustavoSobrera20 <gustavo.senac20@gmail.com>
2024-07-05 10:45:26 -0300

    Add files via upload

---
+++ 7ate100.js
@@ -0,0 +1,23 @@
+contador =0
+
+while (contador < 100 )
+{
+     contador = contador + 1
+     console.log ('7 x', contador, '=', contador * 7);
+
+     if (contador  % 2 == 0) 
+     console.log("Vermelho ")
+    
+     else console.log("Azul")
+     
+      console.log (" ")
+    }
+
+/*contador =0
+
+while (contador < 100 )
+{
+contador = contador + 1
+console.log ('7 x', contador, '=', contador * 7);
+}
+   */
